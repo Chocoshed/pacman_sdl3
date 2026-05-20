774729c85de47ad820afe2ebc88a80713e22226f
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2026-05-20 11:38:09 +0200

    feat: add title, menu, and pause rendering functions to the renderer

--- src/render/renderer.h
+++ src/render/renderer.h
@@ -11,4 +11,7 @@ void draw_pacman(SDL_Renderer *renderer, const Pacman *pacman);
 void draw_ghost(SDL_Renderer *renderer, const Ghost *ghost);
 void draw_hud(SDL_Renderer *renderer, const Score *score);
 void draw_fruit(SDL_Renderer *renderer, const Maze *maze, int level);
-void draw_game_over(SDL_Renderer *renderer);
+void draw_title(SDL_Renderer *renderer);
+void draw_menu(SDL_Renderer *renderer, int selected_option);
+void draw_pause(SDL_Renderer *renderer);
+void draw_game_over(SDL_Renderer *renderer, const Score *score);
